d3644713db3412c4ab741655e3b609ca7cf82c6e
sathish-spidie <sathishshaj9484@gmail.com>
2019-07-12 09:53:36 +0530

    Destructured State with restandspread

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import { useState } from "react";
 import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
   return (
     <div className="App">
       hello
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <div>count1: {count1}</div>
+      <div>count2: {count2}</div>
+      <button
+        onClick={() =>
+          setCount(({ count1, ...rest }) => {
+            return {
+              ...rest,
+              count1: count1 + 1
+            };
+          })
+        }
+      >
+        +
+      </button>
     </div>
   );
 }
